460ed4f14d47b14eedf57d7c3c866f83ac848814
Prasanna Kumar <prasanna.kumar@headforwards.com>
2020-01-26 23:46:08 +0000

    data access tests

---
+++ UserManagementApp/UserManagement.DataAccess.Tests/Entities/AddressTests.cs
@@ -0,0 +1,39 @@
+﻿using Bogus;
+using UserManagement.DataAccess.Entity;
+using Xunit;
+
+namespace UserManagement.DataAccess.Tests.Entities
+{
+    public class AddressTests
+    {
+        [Fact]
+        public void Ctor()
+        {
+            var faker = new Faker("en");
+
+            var organisationId = faker.Random.Number();
+            var addressLine = faker.Random.String();
+            var county = faker.Random.String();
+            var townOrCity = faker.Random.String();
+            var postcode = faker.Random.String();
+            var dateModified = faker.Date.Recent(1);
+
+            var address = new Address
+            {
+                County = county,
+                OrganisationId = organisationId,
+                AddressLine = addressLine,
+                TownOrCity = townOrCity,
+                Postcode = postcode,
+                DateModified = dateModified
+            };
+
+            Assert.Equal(organisationId, address.OrganisationId);
+            Assert.Equal(county, address.County);
+            Assert.Equal(townOrCity, address.TownOrCity);
+            Assert.Equal(addressLine, address.AddressLine);
+            Assert.Equal(postcode, address.Postcode);
+            Assert.Equal(dateModified, address.DateModified);
+        }
+    }
+}
---
+++ UserManagementApp/UserManagement.DataAccess.Tests/Entities/OrganisationTests.cs
@@ -0,0 +1,33 @@
+﻿using Bogus;
+using UserManagement.DataAccess.Entity;
+using Xunit;
+
+namespace UserManagement.DataAccess.Tests.Entities
+{
+    public class OrganisationTests
+    {
+        [Fact]
+        public void Ctor()
+        {
+            var faker = new Faker();
+
+            var organisationName = faker.Random.String();
+            var address = new Address();
+            var licence = faker.Random.String();
+            var dateModified = faker.Date.Recent(1);
+
+            var result = new Organisation
+            {
+                OrganisationName = organisationName,
+                Address = address,
+                Licence = licence,
+                DateModified = dateModified
+            };
+
+            Assert.Equal(organisationName, result.OrganisationName);
+            Assert.Same(address, result.Address);
+            Assert.Equal(licence, result.Licence);
+            Assert.Equal(dateModified, result.DateModified);
+        }
+    }
+}
