6887b3aa069e5c2696f0e3b22955bb64695d3c2a
Prasanna Kumar <dommalapati.chowdary@gmail.com>
2020-03-12 15:42:01 +0000

    ToUserTests

--- UserManagementApp/UserManagement.Business.Tests/Mappings/ToUserTests.cs
+++ UserManagementApp/UserManagement.Business.Tests/Mappings/ToUserTests.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Bogus;
+using System.Collections.Generic;
 using UserManagement.Business.Mappings;
 using UserManagement.Business.Models;
 using UserManagement.DataAccess.Entity;
@@ -29,10 +30,24 @@ namespace UserManagement.Business.Tests.Mappings
                 FirstName = Faker.Name.FirstName(),
                 LastName = Faker.Name.LastName(),
                 Birthday = Faker.Date.Recent(500),
-                DateModified = Faker.Date.Recent(5)
+                DateModified = Faker.Date.Recent(5),
+                Organisations = new List<Organisation> {
+                    new Organisation
+                    {
+                        Id = 10
+                    } 
+                },
+                Roles = new List<UserRole>
+                {
+                    new UserRole
+                    {
+                        Role = nameof(UserRoleTypes.Admin)
+                    }
+                }
             };
             //act
             var userModel = Mapper.Map<UserModel>(user);
+
             //assert
             Assert.Equal(user.Id, userModel.Id);
             Assert.Equal(user.Email, userModel.Email);
@@ -40,6 +55,12 @@ namespace UserManagement.Business.Tests.Mappings
             Assert.Equal(user.LastName, userModel.LastName);
             Assert.Equal(user.Birthday, userModel.Birthday);
             Assert.Equal(user.DateModified, userModel.DateModified);
+
+            Assert.True(userModel.Roles.Count == 1);
+            Assert.Equal(user.Roles[0].Role, userModel.Roles[0].Role);
+
+            Assert.True(userModel.Organisations.Count == 1);
+            Assert.Equal(user.Organisations[0].Id, userModel.Organisations[0].Id);
         }
     }
 }
